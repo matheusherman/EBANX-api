bf78d98ae48c57c5d9c9b1e18f7411a76a639450
Matheus Herman <matheushbandrade@hotmail.com>
2024-05-14 14:52:02 -0300

    heroku

--- src/main/java/com/ebanx/springapi/controller/AccountController.java
+++ src/main/java/com/ebanx/springapi/controller/AccountController.java
@@ -21,56 +21,71 @@ public class AccountController {
     }
 
     @PostMapping("/event")
-    public Object processEvent(@RequestBody Event event) {
+    public ResponseEntity<Object> processEvent(@RequestBody Event event) {
         switch (event.getType()) {
             case "deposit":
-                Account account = accounts.getOrDefault(event.getDestination(), new Account());
-                if (account.getId() == null) {
-                    account.setId(event.getDestination());
-                }
-                account.setBalance(account.getBalance() + event.getAmount());
-                accounts.put(event.getDestination(), account);
-                Map<String, Object> depositResponse = new HashMap<>();
-                depositResponse.put("destination", account);
-                return ResponseEntity.status(HttpStatus.CREATED).body(depositResponse);
-
+                return handleDeposit(event);
             case "withdraw":
-                Account accountWithdraw = accounts.get(event.getOrigin());
-                if (accountWithdraw == null) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(0);
-                }
-                if (accountWithdraw.getBalance() < event.getAmount()) {
-                    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(0);
-                }
-                accountWithdraw.setBalance(accountWithdraw.getBalance() - event.getAmount());
-                accounts.put(event.getOrigin(), accountWithdraw);
-                Map<String, Object> withdrawResponse = new HashMap<>();
-                withdrawResponse.put("origin", accountWithdraw);
-                return ResponseEntity.status(HttpStatus.CREATED).body(withdrawResponse);
+                return handleWithdraw(event);
             case "transfer":
-                Account accountOrigin = accounts.get(event.getOrigin());
-                if (accountOrigin == null) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(0);
-                }
-                if (accountOrigin.getBalance() < event.getAmount()) {
-                    return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(0);
-                }
-                Account accountDestination = accounts.getOrDefault(event.getDestination(), new Account());
-                if (accountDestination.getId() == null) {
-                    accountDestination.setId(event.getDestination());
-                }
-                accountOrigin.setBalance(accountOrigin.getBalance() - event.getAmount());
-                accountDestination.setBalance(accountDestination.getBalance() + event.getAmount());
-                accounts.put(event.getOrigin(), accountOrigin);
-                accounts.put(event.getDestination(), accountDestination);
-                Map<String, Object> transferResponse = new HashMap<>();
-                transferResponse.put("origin", accountOrigin);
-                transferResponse.put("destination", accountDestination);
-                return ResponseEntity.status(HttpStatus.CREATED).body(transferResponse);
+                return handleTransfer(event);
             default:
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid Entry Type");
         }
     }
+
+    private ResponseEntity<Object> handleDeposit(Event event) {
+        Account account = accounts.getOrDefault(event.getDestination(), new Account());
+        if (account.getId() == null) {
+            account.setId(event.getDestination());
+        }
+        account.setBalance(account.getBalance() + event.getAmount());
+        accounts.put(event.getDestination(), account);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("destination", account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    private ResponseEntity<Object> handleWithdraw(Event event) {
+        Account account = accounts.get(event.getOrigin());
+        if (account == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(0);
+        }
+        if (account.getBalance() < event.getAmount()) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(0);
+        }
+        account.setBalance(account.getBalance() - event.getAmount());
+        accounts.put(event.getOrigin(), account);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("origin", account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    private ResponseEntity<Object> handleTransfer(Event event) {
+        Account accountOrigin = accounts.get(event.getOrigin());
+        if (accountOrigin == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(0);
+        }
+        if (accountOrigin.getBalance() < event.getAmount()) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(0);
+        }
+        Account accountDestination = accounts.getOrDefault(event.getDestination(), new Account());
+        if (accountDestination.getId() == null) {
+            accountDestination.setId(event.getDestination());
+        }
+        accountOrigin.setBalance(accountOrigin.getBalance() - event.getAmount());
+        accountDestination.setBalance(accountDestination.getBalance() + event.getAmount());
+        accounts.put(event.getOrigin(), accountOrigin);
+        accounts.put(event.getDestination(), accountDestination);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("origin", accountOrigin);
+        response.put("destination", accountDestination);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 
     @GetMapping("/balance")
     public ResponseEntity<Object> Balance(@RequestParam(value = "account_id") String account_id) {
